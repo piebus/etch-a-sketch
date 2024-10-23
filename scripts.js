8dd34156c802a05a2466bff1279fb86e46c78330
piebus <piers.busser@gmail.com>
2024-10-23 13:04:08 -0700

    Add function to reset square colour to gray

--- scripts.js
+++ scripts.js
@@ -1,16 +1,25 @@
 const container = document.querySelector(".container");
 
+function resetSquares() {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+        square.classList.add("default-square-color");
+    });
+}
+
 function generateGrid(width) {
     for(var i = 0; i < width; i++) {
         const rowOfSqaures = document.createElement("div");
         rowOfSqaures.className = "row";
         for (var j = 0; j < width; j++) {
             const square = document.createElement("div");
-            square.className = "square";
+            square.classList.add("square");
             rowOfSqaures.appendChild(square);
         }
         container.appendChild(rowOfSqaures);
     }
+    resetSquares();
 }
 
-generateGrid(16);
+generateGrid(30);
+resetSquares();
